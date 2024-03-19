2277deb1084086c8e61ab20c3a2f1bb7c5b081a7
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2024-03-19 16:18:24 -0300

    en clase

---
+++ class2/ejercicio1/ejercicio1/ejercicio1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    public class Person
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; }
+        public int Age { 
+            get {
+                DateTime today = DateTime.Today;
+                int age = today.Year - BirthDate.Year;
+                if (BirthDate.Date > today.AddYears(-age))
+                    age--;
+                return age;
+            } 
+        }
+
+    }
+}
